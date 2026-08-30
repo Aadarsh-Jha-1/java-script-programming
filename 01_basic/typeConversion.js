a521129ddc8067f17d5b120d2ca1504bb57ca580
AADARSH JHA <145737043+Aadarsh-Jha-1@users.noreply.github.com>
2026-08-30 04:28:00 +0000

    type conv

---
+++ 01_basic/typeConversion.js
@@ -0,0 +1,14 @@
+score = "a.25k";
+console.log(typeof(score));
+
+let num = Number(score);
+console.log(typeof(score));
+console.log(typeof(num));
+
+console.log(score);
+console.log(num+50);
+
+let data= -35;
+let x = Boolean(data);
+console.log(typeof(data));
+console.log(x);
